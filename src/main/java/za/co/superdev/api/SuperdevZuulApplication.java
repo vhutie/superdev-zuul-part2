2b7303e9cdf0171a7684eb540ed3332a34c0e239
Vutlhari Ndlovhu <vhutie@gmail.com>
2020-11-28 15:50:46 +0200

    init commit

---
+++ src/main/java/za/co/superdev/api/SuperdevZuulApplication.java
@@ -0,0 +1,23 @@
+package za.co.superdev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy  
+@EnableDiscoveryClient  
+@SpringBootApplication
+public class SuperdevZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperdevZuulApplication.class, args);
+	}
+	
+	@Bean
+	  public SimpleFilter simpleFilter() {
+	    return new SimpleFilter();
+	  }
+
+}
---
+++ src/test/java/za/co/superdev/api/SuperdevZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.superdev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperdevZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
